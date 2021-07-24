2ccd20ca3322e47bb36cdea367a9be50719f451d
md jahidul islam <za0162hid@gmail.com>
2021-07-24 18:11:45 +0600

    some change

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -28,7 +28,7 @@ const Sidebar = () => {
       <div className="sidebarItem">
         <span className="sidebarTitle">CATEGORIES</span>
         <ul className="sidebarList">
-          {cats &&
+          {cats ?
             cats.map((c) => (
               <Link to={`/?cat=${c.name}`} className="link">
                 <li className="sidebarListItem">{c.name}</li>
